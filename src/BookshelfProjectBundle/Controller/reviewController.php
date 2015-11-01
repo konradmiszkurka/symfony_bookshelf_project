adf87241f414c0e5d959a9d18d005dbce1c32cf8
kondit <miszkurka.k@gmail.com>
2015-11-01 15:59:22 +0100

    -update reviewController, add (showAction, DeleteAction, AddAction)

--- src/BookshelfProjectBundle/Controller/reviewController.php
+++ src/BookshelfProjectBundle/Controller/reviewController.php
@@ -5,6 +5,13 @@ namespace BookshelfProjectBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Coderslab\BookshelfProjectBundle\Entity\Review;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+/*
+ * @Route("/review")
+ */
 
 class reviewController extends Controller
 {
@@ -12,11 +19,16 @@ class reviewController extends Controller
      * @Route("/show")
      * @Template()
      */
-    public function showAction()
+    public function showAction($reviewId)
     {
+       $repo = $this->getDoctrine()->getRepository("BookshelfProject:Review");
+        $review = $repo->find($reviewId);
+
         return array(
-                // ...
-            );    }
+            "review" => $review
+
+        );
+         }
 
     /**
      * @Route("/add")
@@ -24,18 +36,30 @@ class reviewController extends Controller
      */
     public function addAction()
     {
+        $review = new Review();
+        $form = $this->createFormBuilder($review)
+            ->add("subject", "text")
+            ->add("description", "text")
+            ->add("review", "entity", array("class" => "BookshelfProjectBundle:Book", "choice_label" => "name"))
+            ->add("create", "submit", array("label" => "Add new review"))
+            ->getForm();
         return array(
-                // ...
-            );    }
+            "form" => $form->createView()
+        );
+    }
 
     /**
      * @Route("/delete")
      * @Template()
      */
-    public function deleteAction()
+    public function deleteAction($reviewId)
     {
-        return array(
-                // ...
-            );    }
+        $em = $this->getDoctrine()->getManager();
+
+        $reviewToDelete = $this->getDoctrine()->getRepository("BookshelftProjectBundle:Review")->find($reviewId);
+        $em->remove($reviewToDelete);
+        $em->flush();
+        return $this->redirectToRoute("bookshelfproject_bookshelf_showall");
+    }
 
 }

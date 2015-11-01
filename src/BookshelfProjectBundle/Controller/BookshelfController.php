2fc74ec952b80226a74e3db39e2eeaaf4e35249d
kondit <miszkurka.k@gmail.com>
2015-11-01 15:46:08 +0100

    -update BookshelfController , add(showAllAction, showAction, AddAction, DeleteAction)

--- src/BookshelfProjectBundle/Controller/BookshelfController.php
+++ src/BookshelfProjectBundle/Controller/BookshelfController.php
@@ -5,6 +5,13 @@ namespace BookshelfProjectBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Coderslab\BookshelfProjectBundle\Entity\Bookshelf;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+/**
+ * @Route("/bookshelf")
+ */
 
 class BookshelfController extends Controller
 {
@@ -14,19 +21,28 @@ class BookshelfController extends Controller
      */
     public function showAllAction()
     {
-        return array(
-                // ...
-            );    }
+        {
+            $repo = $this->getDoctrine()->getRepository("BookshelfProjectBundle:Bookshelf");
+            $bookshelf = $repo->findAll();
+            return array(
+                "bookshelf" => $bookshelf
+            );
+        }
+    }
 
     /**
      * @Route("/show")
      * @Template()
      */
-    public function showAction()
+    public function showAction($bookshelfId)
     {
+        $repo = $this->getDoctrine()->getRepository("BookshelfProjectBundle:Bookshelf");
+        $bookshelf = $repo->find($bookshelfId);
+
+
         return array(
-                // ...
-            );    }
+            "bookshelf" => $bookshelf
+        );    }
 
     /**
      * @Route("/Add")
@@ -34,18 +50,27 @@ class BookshelfController extends Controller
      */
     public function AddAction()
     {
+        $bookshelf = new Bookshelf();
+        $form = $this->createFormBuilder($bookshelf)
+                     ->add("name", "text")
+                     ->add("create", "submit", array("label" => "Add new bookshelf"))
+                     ->getForm();
+
         return array(
-                // ...
+                "form" => $form->createView()
             );    }
 
     /**
      * @Route("/Delete")
      * @Template()
      */
-    public function DeleteAction()
+    public function DeleteAction($bookshelfId)
     {
-        return array(
-                // ...
-            );    }
+        $em = $this->getDoctrine()->getManager();
+        $bookshelfToDelete = $this->getDoctrine()->getRepository("BookshelfProjectBundle:Bookshelf")->find($bookshelfId);
+        $em->remove($bookshelfToDelete);
+        $em->flush();
 
+        return $this->redirectToRoute("bookshelfproject_bookshelf_showall");
+}
 }
